88ce88c3419232365c440efce35a4ad887ac4e72
Amr Aly <amr83160@gmail.com>
2024-05-20 02:32:18 +0300

    test

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """
-    User class that inherits from Base Model ss
+    User class that inherits from Base Model
     """
 
     email = ""
