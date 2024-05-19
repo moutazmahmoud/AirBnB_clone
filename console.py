ac57fda32c6e53ab70e0f18a4f9e2630b531848d
Amr Aly <amr83160@gmail.com>
2024-05-20 02:08:49 +0300

    edited

--- console.py
+++ console.py
@@ -41,7 +41,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """EOF command to exit the program."""
-        print()  # Print a newline for better formatting sleep
+        print()  # Print a newline for better formatting
         return True
 
     def emptyline(self):
